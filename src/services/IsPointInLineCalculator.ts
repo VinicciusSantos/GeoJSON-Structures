0e5c27c09cc5a78cfb8731a583a1bd1f4eb78480
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2023-01-28 23:15:18 -0300

    :bug: fix: checking the range when calculate if point is in line

--- src/services/IsPointInLineCalculator.ts
+++ src/services/IsPointInLineCalculator.ts
@@ -6,7 +6,8 @@ export default class IsPointInLineCalculator {
   public calculate(point: Point, line: LineString): boolean {
     const [firstPoint, secondPoint] = line.coordinates
     const matrix = this.preparateMatrix(firstPoint, secondPoint, point.coordinates)
-    return matrix.calculateDeterminant() === 0;
+    const arePoinsAligned = matrix.calculateDeterminant() === 0
+    return arePoinsAligned && this.isPointInInterval(point, line);
   }
 
   private preparateMatrix(firtsPoint: number[], secondPoint: number[], thirdPoint: number[]): Matrix {
@@ -15,5 +16,9 @@ export default class IsPointInLineCalculator {
       [secondPoint[0], secondPoint[1], 1],
       [thirdPoint[0], thirdPoint[1], 1]
     ])
+  }
+
+  private isPointInInterval(point: Point, line: LineString): boolean {
+    return point.coordinates[0] >= line.coordinates[0][0] && point.coordinates[1] <= line.coordinates[1][0]
   }
 }
--- test/services/isPointInLineCalculator.test.ts
+++ test/services/isPointInLineCalculator.test.ts
@@ -3,20 +3,38 @@ import Point from "../../src/entities/Point";
 import IsPointInLineCalculator from "../../src/services/isPointInLineCalculator";
 
 describe("IsPointInLineCalculator tests", () => {
-    const line = new LineString([[-2, -2], [4, 4]])
-    const pointsInLine = [
-        new Point([-1, -1]),
-        new Point([0, 0])
-    ]
-    const pointsOutsideLine = [
-        new Point([30, 1]),
-        new Point([4, 0])
-    ]
+  const line = new LineString([
+    [-2, -2],
+    [4, 4],
+  ]);
+  const pointsInLine = [
+    new Point([-2, -2]),
+    new Point([-1, -1]),
+    new Point([0, 0]),
+    new Point([4, 4]),
+  ];
+  const pointsOutsideLine = [
+    new Point([30, 1]),
+    new Point([4, 0]),
+    new Point([4, 5]),
+    new Point([6, 6]),
+  ];
 
-    const isPointInLineCalculator = new IsPointInLineCalculator()
-    
-    it.each(pointsInLine)('should has point in the line', (point) => {
-        const isPointInLine = isPointInLineCalculator.calculate(point, line)
-        expect(isPointInLine).toBeTruthy()
-    })
+  const isPointInLineCalculator = new IsPointInLineCalculator();
+
+  it.each(pointsInLine)(
+    "should has point $coordinates in the line",
+    (point) => {
+      const isPointInLine = isPointInLineCalculator.calculate(point, line);
+      expect(isPointInLine).toBeTruthy();
+    }
+  );
+
+  it.each(pointsOutsideLine)(
+    "should has point $coordinates outside the line",
+    (point) => {
+      const isPointInLine = isPointInLineCalculator.calculate(point, line);
+      expect(isPointInLine).toBeFalsy();
+    }
+  );
 });
