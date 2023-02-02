54710270fa006c10957c6021ffd1c35f89e8593c
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2023-02-02 11:44:58 -0300

    :sparkles: feat: isPointInPolygon service

--- src/services/IsPointInPolygonCalculator.ts
+++ src/services/IsPointInPolygonCalculator.ts
@@ -1,10 +1,20 @@
 import LineString from "../entities/LineString";
 import Point from "../entities/Point";
 import Polygon from "../entities/Polygon";
-import IsPointInLineCalculator from "./IsPointInLineCalculator";
 
 export default class IsPointInPolygonCalculator {
   public calculate(point: Point, polygon: Polygon): boolean {
-    return true
+    let arestasIntersecting = 0;
+    const horizontalLineToRight = new LineString([[point.x, point.y], [polygon.getExtremePoints().right.x + 1, point.y]]);
+    for (let polyg of polygon.coordinates) {
+      let index = 0
+      for (let point of polyg) {
+        const nextPoint = polyg[index+1] ? polyg[index+1] : polyg[0]
+        const arestaLineString = new LineString([point, nextPoint]);
+        if(arestaLineString.isIntersectingWith(horizontalLineToRight)) arestasIntersecting++;
+        index++;
+      }
+    }
+    return arestasIntersecting % 2 === 1
   }
 }
