77135de255be6ef73a1bd06b45b9fe159881949e
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2023-02-03 08:51:38 -0300

    :sparkles: feat: calculate distance between two points

--- src/entities/Point.ts
+++ src/entities/Point.ts
@@ -3,17 +3,24 @@ import Structure from "./Structure";
 export default class Point extends Structure {
     public x: number;
     public y: number;
+    
     constructor(readonly coordinates: number[]) {
         super('Point', coordinates);
         this.x = this.getX()
         this.y = this.getY()
     }
 
-    getX(): number {
+    public getX(): number {
         return this.coordinates[0]
     }
 
-    getY(): number {
+    public getY(): number {
         return this.coordinates[1]
+    }
+
+    public distanceTo(ohterPoint: Point): number {
+        const distX = Math.pow((ohterPoint.x - this.x), 2)
+        const distY = Math.pow((ohterPoint.y - this.y), 2)
+        return Math.sqrt(distX + distY)
     }
 }
--- test/entities/Point.test.ts
+++ test/entities/Point.test.ts
@@ -8,4 +8,35 @@ describe("Point tests", () => {
         expect(point.type).toBe('Point')
         expect(point.coordinates).toStrictEqual([31.3, 10.8])
     })
+
+    describe('distance between points', () => {
+        const inputs = [
+            {
+                first: [4.8, 7.6],
+                second: [12, 5.5],
+                distance: 7.5
+            },
+            {
+                first: [44.8, 1.8],
+                second: [120, -54.5],
+                distance: 93.94003406429017
+            },
+            {
+                first: [0.8, 4.8],
+                second: [4, 4.15],
+                distance: 3.2653483734511393
+            },
+            {
+                first: [8, 5],
+                second: [7, 5],
+                distance: 1
+            }
+        ];
+        it.each(inputs)("Distance between $first and $second should be close to $distance", (input) => {
+            const first = new Point(input.first)
+            const second = new Point(input.second)
+            const calculatedDistance = first.distanceTo(second)
+            expect(calculatedDistance).toBeCloseTo(input.distance)
+        })
+    })
 });
