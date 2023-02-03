69c015d7715ab42c94c141e6aca0512be534d2a3
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2023-02-03 10:23:20 -0300

    :bug: fix: calculating if point is inside a lot of polygons

--- src/services/IsPointInPolygonCalculator.ts
+++ src/services/IsPointInPolygonCalculator.ts
@@ -7,14 +7,15 @@ export default class IsPointInPolygonCalculator {
     let arestasIntersecting = 0;
     const horizontalLineToRight = new LineString([[point.x, point.y], [polygon.getExtremePoints().right.x + 1, point.y]]);
     for (let polyg of polygon.coordinates) {
-      let index = 0
+      let pointIndex = 0
       for (let point of polyg) {
-        const nextPoint = polyg[index+1] ? polyg[index+1] : polyg[0]
+        const nextPoint = polyg[pointIndex+1] ? polyg[pointIndex+1] : polyg[0]
         const arestaLineString = new LineString([point, nextPoint]);
         if(arestaLineString.isIntersectingWith(horizontalLineToRight)) arestasIntersecting++;
-        index++;
+        pointIndex++;
       }
+      if (arestasIntersecting % 2 === 0 ) return false
     }
-    return arestasIntersecting % 2 === 1
+    return true
   }
 }
--- test/services/IsPointInPolygonCalculator.test.ts
+++ test/services/IsPointInPolygonCalculator.test.ts
@@ -2,29 +2,91 @@ import Point from "../../src/entities/Point";
 import Polygon from "../../src/entities/Polygon";
 import IsPointInPolygonCalculator from "../../src/services/IsPointInPolygonCalculator";
 
-describe('IsPointInPolygon tests', () => {
-    const polygon = new Polygon([[[30, 10], [40, 40], [20, 40], [10, 20]]])
-    const pointsInsidePolygon = [
+describe("IsPointInPolygon tests", () => {
+  interface inputPattern {
+    testTitle: string;
+    polygon: Polygon;
+    pointsInsidePolygon: Point[];
+    pointsOutsidePolygon: Point[];
+  }
+  const inputs: inputPattern[] = [
+    {
+      testTitle: "in simple polygons",
+      polygon: new Polygon([
+        [
+          [30, 10],
+          [40, 40],
+          [20, 40],
+          [10, 20],
+        ],
+      ]),
+      pointsInsidePolygon: [
         new Point([24, 24]),
         new Point([30, 35]),
         new Point([10, 20]),
         new Point([25, 40]),
-    ];
-    const pointsOutsidePolygon = [
+      ],
+      pointsOutsidePolygon: [
         new Point([15, 15]),
         new Point([35, 22]),
         new Point([25, 12]),
         new Point([30, 41]),
-    ];
-    const isPointInPolygonCalculator = new IsPointInPolygonCalculator()
+      ],
+    },
+    {
+      testTitle: "with polygons inside polygons",
+      polygon: new Polygon([
+        [
+          [35, 10],
+          [45, 45],
+          [15, 40],
+          [10, 20],
+          [35, 10],
+        ],
+        [
+          [20, 30],
+          [35, 35],
+          [30, 20],
+          [20, 30],
+        ],
+      ]),
+      pointsInsidePolygon: [
+        new Point([16.12045883056132, 23.84726709443912]),
+        new Point([35.56764937510897, 32.95462551628616]),
+        new Point([15.62058764135378, 39.46682818422099]),
+      ],
+      pointsOutsidePolygon: [
+        new Point([46.59570601881538, 33.188077764036336]),
+        new Point([28.45509314806384, 27.85900277021551]),
+        new Point([10.736065275434441, 38.988247637013615]),
+        new Point([8.215460028902783, 27.76933934694634]),
+      ],
+    },
+  ];
 
-    it.each(pointsInsidePolygon)("should have point $coordinates inside polygon", (point) => {
-        const isPointInPolygon = isPointInPolygonCalculator.calculate(point, polygon)
-        expect(isPointInPolygon).toBeTruthy()
-    })
+  const isPointInPolygonCalculator = new IsPointInPolygonCalculator();
 
-    it.each(pointsOutsidePolygon)("should have point $coordinates outside polygon", (point) => {
-        const isPointInPolygon = isPointInPolygonCalculator.calculate(point, polygon)
-        expect(isPointInPolygon).toBeFalsy()
-    })
+  describe.each(inputs)("$testTitle", (input) => {
+    it.each(input.pointsInsidePolygon)(
+      "should have point $coordinates inside polygon",
+      (point) => {
+        const isPointInPolygon = isPointInPolygonCalculator.calculate(
+          point,
+          input.polygon
+        );
+        expect(isPointInPolygon).toBeTruthy();
+      }
+    );
+
+    it.each(input.pointsOutsidePolygon)(
+      "should have point $coordinates outside polygon",
+      (point) => {
+        const isPointInPolygon = isPointInPolygonCalculator.calculate(
+          point,
+          input.polygon
+        );
+        expect(isPointInPolygon).toBeFalsy();
+      }
+    );
+  });
 });
