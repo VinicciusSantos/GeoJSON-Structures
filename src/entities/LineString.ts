c155ae8a277d4e66bd80c7685011962dfbfbbc97
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2023-02-03 11:59:38 -0300

    :sparkles: feat: line string methods

--- src/entities/LineString.ts
+++ src/entities/LineString.ts
@@ -11,7 +11,34 @@ export default class LineString extends Structure {
   }
 
   public getEndPoint(): Point {
-    return new Point([this.coordinates[1][0], this.coordinates[1][1]]);
+    return new Point([
+      this.coordinates[this.getQuantOfPoints() - 1][0],
+      this.coordinates[this.getQuantOfPoints() - 1][1],
+    ]);
+  }
+
+  public getPointInIndexPosition(indexPosition: number): Point {
+    if (indexPosition + 1 > this.getQuantOfPoints()) {
+      throw new Error("Index of point out of line range!");
+    }
+    return new Point([
+      this.coordinates[indexPosition][0],
+      this.coordinates[indexPosition][1],
+    ]);
+  }
+
+  public getQuantOfPoints(): number {
+    return this.coordinates.length;
+  }
+
+  public getSize(): number {
+    let size = 0;
+    for (let index = 0; index + 1 !== this.coordinates.length; index++) {
+      let point = new Point(this.coordinates[index]);
+      let nextPoint = new Point(this.coordinates[index + 1]);
+      size += point.distanceTo(nextPoint);
+    }
+    return size;
   }
 
   public isIntersectingWith(otherLine: LineString): Point | null {
@@ -19,11 +46,17 @@ export default class LineString extends Structure {
     const p2 = this.getEndPoint();
     const p3 = otherLine.getStartPoint();
     const p4 = otherLine.getEndPoint();
-    if ((p1.x === p2.x && p1.y === p2.y) || (p3.x === p4.x && p3.y === p4.y)) return null;
-    const denominator = (p4.y - p3.y) * (p2.x - p1.x) - (p4.x - p3.x) * (p2.y - p1.y);
+    if ((p1.x === p2.x && p1.y === p2.y) || (p3.x === p4.x && p3.y === p4.y))
+      return null;
+    const denominator =
+      (p4.y - p3.y) * (p2.x - p1.x) - (p4.x - p3.x) * (p2.y - p1.y);
     if (denominator === 0) return null;
-    let ua = ((p4.x - p3.x) * (p1.y - p3.y) - (p4.y - p3.y) * (p1.x - p3.x)) / denominator;
-    let ub = ((p2.x - p1.x) * (p1.y - p3.y) - (p2.y - p1.y) * (p1.x - p3.x)) / denominator;
+    let ua =
+      ((p4.x - p3.x) * (p1.y - p3.y) - (p4.y - p3.y) * (p1.x - p3.x)) /
+      denominator;
+    let ub =
+      ((p2.x - p1.x) * (p1.y - p3.y) - (p2.y - p1.y) * (p1.x - p3.x)) /
+      denominator;
     if (ua < 0 || ua > 1 || ub < 0 || ub > 1) return null;
     let x = p1.x + ua * (p2.x - p1.x);
     let y = p1.y + ua * (p2.y - p1.y);
--- test/entities/LineString.test.ts
+++ test/entities/LineString.test.ts
@@ -2,38 +2,162 @@ import LineString from "../../src/entities/LineString";
 import Structure from "../../src/entities/Structure";
 
 describe("LineString tests", () => {
-    it('should create a LineString', () => {
-        const lineString = new LineString([[31.3, 10.8], [10.3, 20.1]])
-        expect(lineString).toBeInstanceOf(Structure)
-        expect(lineString.type).toBe('LineString')
-        expect(lineString.coordinates).toStrictEqual([[31.3, 10.8], [10.3, 20.1]])
-    })
+  it("should create a LineString", () => {
+    const lineString = new LineString([
+      [31.3, 10.8],
+      [10.3, 20.1],
+    ]);
+    expect(lineString).toBeInstanceOf(Structure);
+    expect(lineString.type).toBe("LineString");
+    expect(lineString.coordinates).toStrictEqual([
+      [31.3, 10.8],
+      [10.3, 20.1],
+    ]);
+  });
 
-    const mainLine = new LineString([[3, 1], [5, 12]])
+  describe("lines intersection", () => {
+    const mainLine = new LineString([
+      [3, 1],
+      [5, 12],
+    ]);
     const linesInstersecting = [
-        new LineString([[2, 6], [6, 6]]),
-        new LineString([[4, 14], [4, 0]]),
-        new LineString([[6, 2], [0, 8]])
-    ]
+      new LineString([
+        [2, 6],
+        [6, 6],
+      ]),
+      new LineString([
+        [4, 14],
+        [4, 0],
+      ]),
+      new LineString([
+        [6, 2],
+        [0, 8],
+      ]),
+    ];
     const linesNotInstersecting = [
-        new LineString([[-4, 2], [-2, 4]]),
-        new LineString([[0, 0], [4, 0]]),
-        new LineString([[10, 2], [6, 12]])
-    ]
+      new LineString([
+        [-4, 2],
+        [-2, 4],
+      ]),
+      new LineString([
+        [0, 0],
+        [4, 0],
+      ]),
+      new LineString([
+        [10, 2],
+        [6, 12],
+      ]),
+    ];
 
-    it.each(linesInstersecting)('should intersect with $coordinates', (otherLine) => {
+    it.each(linesInstersecting)(
+      "should intersect with $coordinates",
+      (otherLine) => {
         const isIntersecting = mainLine.isIntersectingWith(otherLine);
         expect(isIntersecting).toBeTruthy();
-    })
+      }
+    );
 
-    it.each(linesNotInstersecting)('should not intersect with $coordinates', (otherLine) => {
+    it.each(linesNotInstersecting)(
+      "should not intersect with $coordinates",
+      (otherLine) => {
         const isIntersecting = mainLine.isIntersectingWith(otherLine);
         expect(isIntersecting).toBeFalsy();
-    })
+      }
+    );
 
-    it('should not intersect when lines have no size', () => {
-        const firstLine = new LineString([[1, 2], [1, 2]])
-        const secondLine = new LineString([[2, 4], [2, 4]])
-        expect(firstLine.isIntersectingWith(secondLine)).toBeFalsy()
-    })
+    it("should not intersect when lines have no size", () => {
+      const firstLine = new LineString([
+        [1, 2],
+        [1, 2],
+      ]);
+      const secondLine = new LineString([
+        [2, 4],
+        [2, 4],
+      ]);
+      expect(firstLine.isIntersectingWith(secondLine)).toBeFalsy();
+    });
+  });
+
+  describe("position methods", () => {
+    const inputs = [
+      {
+        lineCoordinates: [
+          [1, 4],
+          [7, 5],
+          [6, 5],
+          [8, 2],
+        ],
+        startPoint: [1, 4],
+        endPoint: [8, 2],
+        qtdPoints: 4,
+        size: 10.688313805762208,
+      },
+      {
+        lineCoordinates: [
+          [24, 7],
+          [4, 0],
+          [0, 4],
+          [8, 7],
+          [4, 4],
+        ],
+        startPoint: [24, 7],
+        endPoint: [4, 4],
+        qtdPoints: 5,
+        size: 40.390478095227,
+      },
+      {
+        lineCoordinates: [
+          [35, 12],
+          [0, 0],
+          [8, 7],
+        ],
+        startPoint: [35, 12],
+        endPoint: [8, 7],
+        qtdPoints: 3,
+        size: 47.63014581273465,
+      },
+    ];
+
+    describe("getStartPoint", () => {
+      it.each(inputs)("start point should be $startPoint", (input) => {
+        const lineString = new LineString(input.lineCoordinates);
+        const startPoint = lineString.getStartPoint().coordinates;
+        expect(startPoint).toStrictEqual(input.startPoint);
+      });
+    });
+
+    describe("getEndPoint", () => {
+      it.each(inputs)("end point should be $endPoint", (input) => {
+        const lineString = new LineString(input.lineCoordinates);
+        const endPoint = lineString.getEndPoint().coordinates;
+        expect(endPoint).toStrictEqual(input.endPoint);
+      });
+    });
+
+    describe("getQuantOfPoints", () => {
+      it.each(inputs)("should have $qtdPoints points", (input) => {
+        const lineString = new LineString(input.lineCoordinates);
+        const qtdPoints = lineString.getQuantOfPoints();
+        expect(qtdPoints).toBe(input.qtdPoints);
+      });
+    });
+
+    describe("getSize", () => {
+      it.each(inputs)("should have size $size", (input) => {
+        const lineString = new LineString(input.lineCoordinates);
+        const size = lineString.getSize();
+        expect(size).toBeCloseTo(input.size);
+      });
+    });
+
+    describe("getPointInIndexPosition", () => {
+      it.each(inputs)("should get the elements", (input) => {
+        const lineString = new LineString(input.lineCoordinates);
+        let index = 0
+        for (let point of input.lineCoordinates) {
+            expect(lineString.getPointInIndexPosition(index).coordinates).toStrictEqual(point);
+        }
+      });
+    });
+  });
 });
